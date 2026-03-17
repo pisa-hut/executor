84daf3a8fd333a00c4aa4cba4c4b61a2a2fb8010
LolaInTa Chi <lolainta@gmail.com>
2026-03-17 21:50:38 +0800

    test: add aw/ca-frankenburg script

---
+++ scripts/all.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --job-name=all
+#SBATCH --output=outputs/stdout/job_%j
+#SBATCH --error=outputs/stderr/job_%j
+#SBATCH --time=01:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=12G
+
+source "$SLURM_SUBMIT_DIR/scripts/utils.sh"
+header
+
+echo "Starting executor"
+uv run -m executor
+
+if [ $? -ne 0 ]; then
+    echo "Executor execution failed."
+    exit 1
+fi
+
+echo "End Time:      $(date)"
--- scripts/utils.sh
+++ scripts/utils.sh
@@ -4,6 +4,8 @@ function header() {
     echo "=== SLURM TEST JOB ==="
     echo "Cluster:       $SLURM_CLUSTER_NAME"
     echo "Job ID:        $SLURM_JOB_ID"
+    echo "Array ID:      $SLURM_ARRAY_TASK_ID"
+    echo "Job Array ID:  $SLURM_ARRAY_JOB_ID"
     echo "Partition:     $SLURM_JOB_PARTITION"
     echo "Job Name:      $SLURM_JOB_NAME"
     echo "Node List:     $SLURM_NODELIST"
